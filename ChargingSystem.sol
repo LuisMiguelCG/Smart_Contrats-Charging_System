1b404e1ed167aeb6ba15e6691c4469033a67e372
LuisMiguelCG <luismiguel-lmcg@hotmail.com>
2026-03-23 13:36:34 +0100

    Actualizar ChargingSystem.sol

--- ChargingSystem.sol
+++ ChargingSystem.sol
@@ -2,9 +2,8 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract ChargingSystem {
-
     uint256 nChargers;
-    uint256 pricePerTime; 
+    uint256 pricePerTime;
     uint256 maxTime;
     uint256 minTime;
     uint32 busyChargersFlag;
@@ -40,7 +39,7 @@ contract ChargingSystem {
     constructor(uint256 _nChargers, uint256 _pricePerTime, address _admin) {
         require(_nChargers <= 32, "Max 32 chargers");
         nChargers = _nChargers;
-        pricePerTime = _pricePerTime;  
+        pricePerTime = _pricePerTime;
         admin = _admin;
         maxTime = 2 hours;
         minTime = 15 minutes;
@@ -54,19 +53,19 @@ contract ChargingSystem {
     function log2(uint32 x) internal pure returns (uint32) {
         uint32 r = 0;
 
-        if (x >= 2**16) {
+        if (x >= 2 ** 16) {
             x >>= 16;
             r += 16;
         }
-        if (x >= 2**8) {
+        if (x >= 2 ** 8) {
             x >>= 8;
             r += 8;
         }
-        if (x >= 2**4) {
+        if (x >= 2 ** 4) {
             x >>= 4;
             r += 4;
         }
-        if (x >= 2**2) {
+        if (x >= 2 ** 2) {
             x >>= 2;
             r += 2;
         }
@@ -94,15 +93,10 @@ contract ChargingSystem {
         return _maskBusy == 0 ? 0 : log2(_maskBusy & (~_maskBusy + 1));
     }
 
-    function addNewUser(uint256 _time) internal returns (bool){
+    function addNewUser(uint256 _time) internal returns (bool) {
         uint32 idCharger = _firstChargerAvailableId(busyChargersFlag);
 
-        UserToCharger[msg.sender] = Charger(
-            msg.sender,
-            idCharger,
-            block.timestamp,
-            block.timestamp + _time
-        );
+        UserToCharger[msg.sender] = Charger(msg.sender, idCharger, block.timestamp, block.timestamp + _time);
         IdToAddr[idCharger] = msg.sender;
         _setBusyCharger(idCharger);
         busyChargersNum += 1;
@@ -110,42 +104,38 @@ contract ChargingSystem {
         address minUser = minTimeUser.addr;
         address nextMinUser = minTimeUser.next_user;
 
-        uint256 minUserStop = minUser != address(0x0) 
-            ? UserToCharger[minUser].stopTime 
-            : 0;
-        uint256 nextMinUserStop = nextMinUser != address(0x0) 
-            ? UserToCharger[nextMinUser].stopTime 
-            : 0;
+        uint256 minUserStop = minUser != address(0x0) ? UserToCharger[minUser].stopTime : 0;
+        uint256 nextMinUserStop = nextMinUser != address(0x0) ? UserToCharger[nextMinUser].stopTime : 0;
         uint256 userStop = UserToCharger[msg.sender].stopTime;
 
         if (minUserStop == 0) {
             minTimeUser.addr = msg.sender;
             minTimeUser.next_user = address(0x0);
-            emit ChargerStarted(msg.sender,_time);
+            emit ChargerStarted(msg.sender, _time);
             return true;
         }
 
         if (userStop < minUserStop) {
             minTimeUser.next_user = minUser;
             minTimeUser.addr = msg.sender;
-            emit ChargerStarted(msg.sender,_time);
+            emit ChargerStarted(msg.sender, _time);
             return true;
         }
 
         if (nextMinUserStop == 0 || userStop < nextMinUserStop) {
             minTimeUser.next_user = msg.sender;
-            emit ChargerStarted(msg.sender,_time);
+            emit ChargerStarted(msg.sender, _time);
             return true;
         }
 
         if (userStop > nextMinUserStop) {
-            emit ChargerStarted(msg.sender,_time);
+            emit ChargerStarted(msg.sender, _time);
             return true;
         }
         return false;
     }
 
-    function removeMinUser() internal returns(bool){
+    function removeMinUser() internal returns (bool) {
         address minUser = minTimeUser.addr;
         address minNextUser = minTimeUser.next_user;
         uint32 idCharger = UserToCharger[minUser].chargerId;
@@ -186,12 +176,10 @@ contract ChargingSystem {
         return false;
     }
 
-
     function refreshBusyChargers() internal returns (bool) {
         uint32 busyFlag = busyChargersFlag;
-        Charger memory minUser = minTimeUser.addr != address(0) 
-            ? UserToCharger[minTimeUser.addr] 
-            : Charger(address(0x0),0,0,0);
+        Charger memory minUser =
+            minTimeUser.addr != address(0x0) ? UserToCharger[minTimeUser.addr] : Charger(address(0x0), 0, 0, 0);
 
         if (busyFlag == 0) {
             return false;
@@ -206,11 +194,11 @@ contract ChargingSystem {
     }
 
     function withdrawFunds() external onlyAdmin {
-        (bool result, ) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool result,) = payable(msg.sender).call{value: address(this).balance}("");
         require(result, "Error depositing money");
     }
 
-    function startCharge() external payable returns(uint32){
+    function startCharge() external payable returns (uint32) {
         require(msg.value != 0, "Invalid Value (value)");
         uint256 amount = msg.value;
 
@@ -219,7 +207,7 @@ contract ChargingSystem {
 
         refreshBusyChargers();
         require(busyChargersNum < nChargers, "No Chargers Available");
-   
+
         bool successful = addNewUser(time);
         require(successful, "Error adding user");
 
